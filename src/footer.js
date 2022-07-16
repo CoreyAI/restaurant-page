41f85df7be4400ba18fa4edc803456baa08a3d65
CoreyAI <coreyiaquinta@gmail.com>
2022-07-16 14:03:20 -0400

    Added footer

---
+++ src/footer.js
@@ -0,0 +1,12 @@
+const Footer = (() => {
+  const template = `
+    <footer class="footer">
+      <div>footer content goes here</div>
+    </footer>
+  `;
+
+  return {template};
+})();
+
+export {Footer};
+
